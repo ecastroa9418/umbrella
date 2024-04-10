3d186b8ea7048196a2ce8595ab5d2353eebddf13
ecastroa9418 <165231064+ecastroa9418@users.noreply.github.com>
2024-04-10 21:04:23 +0000

    Problem Set 1 with next hour summary

--- umbrella.rb
+++ umbrella.rb
@@ -2,19 +2,19 @@ require "http"
 require "json"
 
 pp "Where are you located?"
-#user_location = gets.chomp.gsub(" ", "%20")
-user_location = "Chicago"
+user_location = gets.chomp.gsub(" ", "%20")
+
 pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
 resp = HTTP.get(maps_url)
 
-raw_response = resp.to_s
+raw_response_maps = resp.to_s
 
-parsed_response = JSON.parse(raw_response)
+parsed_response_maps = JSON.parse(raw_response_maps)
 
-results = parsed_response.fetch("results")
+results = parsed_response_maps.fetch("results")
 
 first_result = results.at(0)
 
@@ -22,30 +22,29 @@ geo = first_result.fetch("geometry")
 
 loc = geo.fetch("location")
 
-pp latitued = loc.fetch("lat")
-pp longitude =  loc.fetch("lng")
+latitude = loc.fetch("lat")
+longitude = loc.fetch("lng")
 
-
-
-=begin
-rescue => exception
-  
-end
-# Hidden variables
 pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
 
 # Assemble the full URL string by adding the first part, the API token, and the last part together
-pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/#{latitude},#{longitude}"
 
 # Place a GET request to the URL
-raw_response = HTTP.get(pirate_weather_url)
+raw_response_weather = HTTP.get(pirate_weather_url)
 
-parsed_response = JSON.parse(raw_response)
+parsed_response_weather = JSON.parse(raw_response_weather)
 
-currently_hash = parsed_response.fetch("currently")
+currently_hash = parsed_response_weather.fetch("currently")
 
 current_temp = currently_hash.fetch("temperature")
 
-puts "The current temperature is " + current_temp.to_s + "."
+next_hour_hash = parsed_response_weather.fetch("hourly")
 
-=end
+next_hour_array = next_hour_hash.fetch("data")
+
+next_hour_hash = next_hour_array.at(0)
+
+next_hour_summary = next_hour_hash.fetch("summary")
+
+puts "It is currently F°#{current_temp} and in the next hour will be #{next_hour_summary}"
